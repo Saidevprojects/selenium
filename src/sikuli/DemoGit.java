053efd5c542f5818262921ea6b8d1e73053e5e2b
Sai Krishna Munirathinam <saikrishna20021998@gmail.com>
2026-03-27 20:33:44 +0530

    Demoupdate2

--- src/sikuli/DemoGit.java
+++ src/sikuli/DemoGit.java
@@ -8,7 +8,7 @@ import org.testng.annotations.BeforeTest;
 public class DemoGit {
 	WebDriver driver;
   @Test
-  public void f() {
+  public void google() {
 	  driver.get("https://www.google.com/");
 	  
   }
