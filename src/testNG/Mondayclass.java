41e91a54eb3e35edb0f9b6289f0aa125e4b78264
Sai Krishna Munirathinam <saikrishna20021998@gmail.com>
2026-03-30 20:44:23 +0530

    Monday class updated

--- src/testNG/Mondayclass.java
+++ src/testNG/Mondayclass.java
@@ -1,14 +1,21 @@
 package testNG;
 
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Mondayclass {
-  @Test
-  public void f() {
+	WebDriver driver;  
+	@Test
+  public void google() {
+			  driver.get("https://www.google.com");
   }
   @BeforeTest
   public void beforeTest() {
+	  
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
